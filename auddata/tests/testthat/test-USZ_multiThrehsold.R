ab29ce83cf4dd0b801321e3b21acf518713b01de
Pia Neuschwander <pia.neuschwander@gmx.ch>
2020-07-07 17:28:51 +0200

    changed testing

--- auddata/tests/testthat/test-USZ_multiThrehsold.R
+++ auddata/tests/testthat/test-USZ_multiThrehsold.R
@@ -4,10 +4,15 @@ test_that("Errored as expected.", {
   expect_error(extract_thresholddata("Astring"))
 })
 
+aud_filelist <- system.file(..., package = "base", lib.loc = NULL, mustWork = FALSE)(path = "/home/neuschwanderp/Desktop/Docs/R/gitHub/USZ_multiThreshold/auddata/ex_data/", full.names = TRUE, recursive = TRUE)
+
+threshold_data <- extract_thresholddata(aud_filelist)
+
+
 test_that("Is numeric", {
-  expect_that(data_audio$Runs, is.numeric())
+  expect_that(threshold_data$Runs, is.numeric())
 })
 
 test_that("Is numeric", {
-  expect_that(data_audio$Thresholds, is.numeric())
+  expect_that(threshold_data$Thresholds, is.numeric())
 })
