52d400fe6dd5b64c14ebedfbab0ac61cfb0c727e
Pia Neuschwander <pia.neuschwander@gmx.ch>
2020-07-07 17:53:19 +0200

    changed test structure

--- auddata/tests/testthat/test-USZ_multiThrehsold.R
+++ auddata/tests/testthat/test-USZ_multiThrehsold.R
@@ -1,18 +1,24 @@
 context("general testing")
 
 test_that("Errored as expected.", {
-  expect_error(extract_thresholddata("Astring"))
+  expect_error(extract_thresholddata("stringinput"))
 })
 
-aud_filelist <- system.file(..., package = "base", lib.loc = NULL, mustWork = FALSE)(path = "/home/neuschwanderp/Desktop/Docs/R/gitHub/USZ_multiThreshold/auddata/ex_data/", full.names = TRUE, recursive = TRUE)
+#aud_filelist <- system.file("Summary_0bsr6v EAR Thr 25-Jul-2018 13_15_41", package = "auddata", lib.loc = NULL, mustWork = FALSE)
+#threshold_data <- extract_thresholddata(aud_filelist)
 
+aud_filelist <- list.files(path = "/home/neuschwanderp/Desktop/Docs/R/gitHub/USZ_multiThreshold/auddata/ex_data/", full.names = TRUE, recursive = TRUE)
 threshold_data <- extract_thresholddata(aud_filelist)
 
-
 test_that("Is numeric", {
-  expect_that(threshold_data$Runs, is.numeric())
+  expect_that(is.numeric(threshold_data$Runs), is_true())
 })
 
 test_that("Is numeric", {
-  expect_that(threshold_data$Thresholds, is.numeric())
+  expect_that(is.numeric(threshold_data$Runs), is_true())
 })
+
+test_that("Is character", {
+  expect_that(is.character(threshold_data$name), is_true())
+})
+
